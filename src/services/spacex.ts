2770926918872d36688bcf2579aebf5b84d7b915
jmquintana <mailjmq@gmail.com>
2024-02-03 11:17:31 -0300

    first commit

---
+++ src/services/spacex.ts
@@ -0,0 +1,34 @@
+import { type Doc, type APISpaceXResponse } from "../types/api.ts";
+
+export const getLaunchBy = async ({ id }: { id: string }) => {
+  const res = await fetch(`https://api.spacexdata.com/v5/launches/${id}`);
+
+  const launch = (await res.json()) as Doc;
+
+  return launch;
+};
+
+export const getLatestLaunches = async (
+  { page = 1 }: { page: number } = { page: 1 }
+) => {
+  const res = await fetch("https://api.spacexdata.com/v5/launches/query", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: {},
+      options: {
+        sort: {
+          date_unix: "asc",
+        },
+        limit: 12,
+        page: page,
+      },
+    }),
+  });
+
+  const { docs: launches } = (await res.json()) as APISpaceXResponse;
+
+  return launches;
+};
